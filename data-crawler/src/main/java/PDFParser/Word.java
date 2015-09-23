aec3c38daecbe40f0476973be287435f61738a42
Nick Barban <nn.barban@gmail.com>
2015-09-23 19:24:41 +0300

    Add PDFParser2Json classes

---
+++ data-crawler/src/main/java/PDFParser/Word.java
@@ -0,0 +1,23 @@
+package PDFParser;
+
+
+
+/**
+ * Class Word, used to convert in Json format
+ * @author Michel Tu <orphee@gmail.com>
+ * @version 1.0
+ */
+
+public class Word {
+	private String word;
+	private int x;
+	private int width;
+	
+	
+	public Word(String word, int startX, int width) {
+		this.word = word;
+		this.x = startX;
+		this.width = width;
+	}
+
+}
